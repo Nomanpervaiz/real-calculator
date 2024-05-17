bf3094ab800c90da536c703585606cebdaad8789
Nomanpervaiz <nomanpervaiz035@gmail.com>
2024-05-17 18:32:10 +0500

    add files

--- app.js
+++ app.js
@@ -1,64 +1,106 @@
+let display = document.getElementById("display");
+let screenCopy = document.querySelector(".screenCopy");
+let equalSign = document.getElementById("equalTo");
+let historyList = document.getElementById("list");
 
-let display = document.getElementById("display")
-let screenCopy = document.querySelector('.screenCopy')
 // clear display function
 function clearValue() {
   display.value = "";
-  screenCopy.value =  "";
-
+  screenCopy.value = "";
 }
+
 // Delete value one by one
 function deleteValue() {
   display.value = display.value.slice(0, -1);
 }
 
-
 // number function 0 to 9
+let lastInputWasOperator = false;
 function valueGet(value) {
-    let screenValue = display.value += value;
-  screenCopy.value = screenValue
-} 
+  if (lastInputWasOperator && ["+", "-", "*", "/", "%"].includes(value)) {
+    return;
+  }
 
+  if (!["+", "-", "*", "/", "%"].includes(value)) {
+    lastInputWasOperator = false;
+  }
+  display.value += value;
+  screenCopy.value += value;
+  if (["+", "-", "*", "/", "%"].includes(value)) {
+    lastInputWasOperator = true;
+  }
+}
 
 // expressions
 function results() {
-    let expression = display.value;
-  let match = expression.match((/^(\d+)([+\-x÷%])(\d+)$/))
-  if (match) {
-      let num1 = parseFloat(match[1]);
-      let operator = match[2];
-      let num2 = parseFloat(match[3]);
-      let result;
-      switch (operator) {
-          case '+':
-              result = num1 + num2;
-              console.log(result);
-              break;
-          case '-':
-              result = num1 - num2;
-              console.log(result);
-              break;
-          case 'x':
-              result = num1 * num2;
-              console.log(result);
-              break;
-          case '%':
-              result = num1 % num2;
-              console.log(result);
-              break;
-          case '÷':
-              if (num2 !== 0) {
-                  result = num1 / num2;
-                  console.log(result);
-              } else {
-                  result = "syntx error";
-              }
-              break;
-          default:
-              result = "syntx error";
+  let expression = display.value;
+  let str = expression;
+  let result = "";
+  historyList.innerHTML += "<li>" + "Expression : " + str + "</li>";
+  if (str) {
+    let operator = "";
+    let num1 = "";
+    let num2 = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str.indexOf("+") !== -1) {
+        operator = str.indexOf("+");
+      } else if (str.indexOf("*") !== -1) {
+        operator = str.indexOf("*");
+      } else if (str.indexOf("/") !== -1) {
+        operator = str.indexOf("/");
+      } else if (str.indexOf("-") !== -1) {
+        operator = str.indexOf("-");
+      } else {
+        console.log("invalid operator");
       }
-      display.value = result;
+
+      if (str.slice(0, operator.length)) {
+        num1 = str.slice(0, operator);
+      } else {
+        num1 = "";
+      }
+      if (str.slice(operator + 1, i + 1)) {
+        num2 = str.slice(operator + 1, i + 1);
+      } else {
+        num2 = "";
+      }
+    }
+
+    operator = str[operator];
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        historyList.innerHTML += "<li>" + "Result : " + result + "</li> <br>";
+        break;
+      case "-":
+        result = num1 - num2;
+        historyList.innerHTML += "<li>" + "Result : " + result + "</li> <br>";
+        break;
+      case "*":
+        result = num1 * num2;
+        historyList.innerHTML += "<li>" + "Result : " + result + "</li> <br>";
+        break;
+      case "/":
+        if (num2 !== 0) {
+          result = num1 / num2;
+          historyList.innerHTML += "<li>" + "Result : " + result + "</li> <br>";
+        } else {
+          result = "Not divisible!";
+        }
+        break;
+      case "%":
+        result = num1 % num2;
+        historyList.innerHTML += "<li>" + "Result : " + result + "</li> <br>";
+        break;
+
+      default:
+        result = "Invalid operator!";
+        break;
+    }
+    display.value = result;
   } else {
-      display.value = "Error: Invalid ";
+    result = "Syntax Error!";
   }
 }
